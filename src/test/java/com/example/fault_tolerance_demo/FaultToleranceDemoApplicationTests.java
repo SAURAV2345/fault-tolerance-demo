f439ea51b1a580796e7f4ece668b7d3ec8f89084
Choudhary <schoudha@informatica.com>
2025-12-10 11:28:03 +0530

    Fault Tolerance basic project setup

---
+++ src/test/java/com/example/fault_tolerance_demo/FaultToleranceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fault_tolerance_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaultToleranceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
